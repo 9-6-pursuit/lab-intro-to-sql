f09100cded3099c407d44941ad0f6d5508fcccb6
tshering gurung <tshering@tsherings-air.myfiosgateway.com>
2023-07-12 22:23:50 -0400

    SQL lab

--- regifter.sql
+++ regifter.sql
@@ -15,51 +15,59 @@ CREATE DATABASE regifter;
 -- giver - string
 -- value - integer
 -- previously_regifted boolean
-
+CREATE TABLE gifts (id SERIAL PRIMARY KEY, gift TEXT,giver TEXT, value INT, previously_regifted BOOLEAN );
 
 -- 
 \echo See details of the table you created
 -- 
+/d gifts
 
 
 -- 
-\echo Alter the table so that the column price is changed to value 
+\echo Alter the table so that the column value is changed to price 
 -- 
-
+ALTER TABLE gifts RENAME value to price;
 
 -- 
 \echo Insert a peach candle, given by 'Santa' thats value is 9 and has been previously regifted
 -- 
-
+INSERT INTO gifts (gift, giver, value, previously_regifted) VALUES ('peach candle', 'Santa', 9, TRUE);
 
 --
 \echo Query for all the columns in your gifts table
 -- 
-
+SELECT * FROM gifts
 
 --
 \echo Uncomment below to insert 5 more gifts
 -- 
 
--- INSERT INTO gifts (gift, giver, value, previously_regifted)
--- VALUES
--- ('peach candle', 'Santa', '9', TRUE),
--- ('cinnamon candle', 'Nick', '19', TRUE),
--- ('soap on a rope', 'Rudolf', '29', FALSE),
--- ('potpurri', 'Elf on the Shelf', '39', TRUE),
--- ('mango candle', 'The Boss', '49', FALSE)
--- ;
+INSERT INTO gifts (gift, giver, value, previously_regifted)
+VALUES
+('peach candle', 'Santa', '9', TRUE),
+('cinnamon candle', 'Nick', '19', TRUE),
+('soap on a rope', 'Rudolf', '29', FALSE),
+('potpurri', 'Elf on the Shelf', '39', TRUE),
+('mango candle', 'The Boss', '49', FALSE)
+;
 
 -- 
 \echo Insert 5 more gifts of your own choosing,  include 1 more candle
 --
-
+INSERT INTO gifts (gift, giver, value, previously_regifted)
+VALUES
+('candy box', 'Anita', '12', FALSE),
+('guitar', 'Vick', '21', TRUE),
+('soap', 'Rudy', '29', FALSE),
+('pot', 'Eugene', '39', TRUE),
+('vase', 'Sally', '49', FALSE)
+;
 
 
 --
 \echo Query for gifts with a price greater than or equal to 20
 --
-
+SELECT * FROM gifts WHERE price = 20 ;
 
 --
 \echo Query for every gift that has the word candle in it, only show the gift column
